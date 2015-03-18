955d7f230e681423728d9ab8b28e62ed8b6e6b32
Thomas de Barochez <tdebarochez@users.noreply.github.com>
2015-03-18 10:34:24 +0100

    php-5.6.6

--- Formula/php56.rb
+++ Formula/php56.rb
@@ -5,9 +5,9 @@ require 'formula'
 
 class Php56 < AbstractPhp
   init
-  url "http://php.net/get/php-5.6.5.tar.bz2/from/this/mirror"
-  sha1 "a523a13110a66f020c36f088089d2c5c7de9f6a9"
-  version "5.6.5"
+  url "http://php.net/get/php-5.6.6.tar.bz2/from/this/mirror"
+  sha1 "6795db456f66193634ff38cf8039636b168eda3c"
+  version "5.6.6"
   def php_version
     5.6
   end
