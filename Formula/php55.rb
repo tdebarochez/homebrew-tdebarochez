35b0e5d28926e0b57e12e3db1252aed155262792
Thomas de Barochez <tdebarochez@users.noreply.github.com>
2015-03-23 09:49:34 +0100

    php-5.5.23

--- Formula/php55.rb
+++ Formula/php55.rb
@@ -5,9 +5,9 @@ require 'formula'
 
 class Php55 < AbstractPhp
   init
-  url "http://php.net/get/php-5.5.21.tar.bz2/from/this/mirror"
-  sha1 "a6251bb3565f144593df4329fd15bedd70ae13a8"
-  version "5.5.21"
+  url "http://php.net/get/php-5.5.23.tar.bz2/from/this/mirror"
+  sha1 "a206cc931bd3aec946a7e5f82b28552d4a93c3cb"
+  version "5.5.23"
   def php_version
     5.5
   end
