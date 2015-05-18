489004bb5a4f96e13a80e8df0036481c230d7293
Thomas de Barochez <tdebarochez@users.noreply.github.com>
2015-05-18 10:25:34 +0200

    php54, s/sha1/sha256/

--- Formula/php54.rb
+++ Formula/php54.rb
@@ -6,7 +6,7 @@ require 'formula'
 class Php54 < AbstractPhp
   init
   url "http://php.net/get/php-5.4.41.tar.bz2/from/this/mirror"
-  sha1 "5bc4b45a1280ff80a3cf5b8563716f325cfd0121d7fd25aa54d56ff38b3b8272"
+  sha256 "5bc4b45a1280ff80a3cf5b8563716f325cfd0121d7fd25aa54d56ff38b3b8272"
   version "5.4.41"
   def php_version
     5.4
