4da9d31d522fd77e26c0b7061bf82626f987ede3
Thomas de Barochez <tdebarochez@users.noreply.github.com>
2015-05-18 10:18:57 +0200

    php-5.4.41

--- Formula/php54.rb
+++ Formula/php54.rb
@@ -5,9 +5,9 @@ require 'formula'
 
 class Php54 < AbstractPhp
   init
-  url "http://php.net/get/php-5.4.39.tar.bz2/from/this/mirror"
-  sha1 "b7ae0472a9a6fdd6b1e502650c971717b77d4f84"
-  version "5.4.39"
+  url "http://php.net/get/php-5.4.41.tar.bz2/from/this/mirror"
+  sha1 "5bc4b45a1280ff80a3cf5b8563716f325cfd0121d7fd25aa54d56ff38b3b8272"
+  version "5.4.41"
   def php_version
     5.4
   end
